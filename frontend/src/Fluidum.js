2bab028ebe5ab6973e0a5af8dd6f07a3c121a826
Michael Busitllos <mdbust24@icloud.com>
2020-12-27 18:19:16 -0800

    Added Route

--- frontend/src/Fluidum.js
+++ frontend/src/Fluidum.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Provider } from "./Context";
+import Home from './routes/Home';
+import Profile from './routes/Profile';
 import './Fluidum.css';
 
 export default function Fluidum() {
   return (
-    <div className="Fluidum">
-      hello
-    </div>
+    <Provider
+			value={{ }}>
+      <Router>
+        <div className="body">
+          {/* Top navigation bar */}
+          <nav className="topNav">
+            {/* Homepage link */}
+            <div className="homeLink">
+              <Link to="/">Home</Link>
+            </div>
+            {/* Profile link */}
+            <div className="profileLink">
+              <Link to="/Profile">Profile</Link>
+            </div>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/Profile">
+              <Profile 
+              />
+            </Route>
+            <Route path="/">
+              <Home 
+              />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+		</Provider>
   );
 }
 
